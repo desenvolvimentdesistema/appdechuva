07a712545dd8f5785f9a09bef48244c6aaf93f71
AppDeTempo <juaugustiveiga@gmail.com>
2024-04-18 11:47:23 -0300

    ...........

---
+++ Result.cs
@@ -0,0 +1,6 @@
+public class Results
+{
+    public int temp
+Main.Page.xaml
+<Label x:Name="Label Temp"
+}
---
+++ appdetempo/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace appdetempo;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}

1a9bd6b4b97b698dca6ffe927934d86cfa7f4af4
luna <juauguseveiga@gmail.com>
2024-04-22 09:10:11 -0300

    .

--- appdetempo/MainPage.xaml.cs
+++ appdetempo/MainPage.xaml.cs
@@ -6,17 +6,17 @@ public partial class MainPage : ContentPage
 {
 	int count = 0;
 
-    public MainPage()
+    public MainPage();
 	
  	InitializeComponent();
-    void Testa Layout()
+    void Testa Layout();
 	{
-		Results = new Results()
+		Results = new Results();
 		Results.Temp=23;
 		Results.Description="Sol";
 		Results.Rain=10;
 	}
-	void Preencher Tela()
+	void Preencher Tela();
 	{
 		LabelChuva.Text=Results.Rain;
 		LabelUmidade.Text=Results.Humidity;
